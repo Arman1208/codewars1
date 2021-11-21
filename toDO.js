079bbff5bae0dce9e247fd2307495626f5b3483e
Arman1208 <kitarov.uralsk@gmail.com>
2021-11-21 18:29:37 +0100

    added toDo task project

---
+++ toDO.js
@@ -0,0 +1,18 @@
+// let query= prompt('choose your option')
+// const list=['sample','sample1']
+// while(query!=='quit'){
+//     if(query==='list'){
+//         for(let i=0;i<list.length;i++){
+//             console.log(`${i} ${list[i]}`)
+//         }
+//     }else if(query==='new'){
+//         const word=prompt('enter your ToDO')
+//         list.push(word)
+//         console.log(`${word} added to list`)
+//     }else if(query==='delete'){
+//         let del1=parseInt(prompt('which index you wanna delete'))
+//         list.splice(del1,1)
+//     }
+//     query=prompt('what do you want?')
+// }
+// console.log('quitting it')
