103d8cc144e9b65b2ca42a53b7ce2f87a15b1e6b
Arman1208 <kitarov.uralsk@gmail.com>
2021-11-23 23:32:43 +0100

    added find missing key

---
+++ Find the missing letter.js
@@ -0,0 +1,16 @@
+function findMissingLetter(array)
+{
+  for (let i = 1; i < array.length; i++) {
+    // get the char code of the previous letter
+    const prev = array[i - 1].charCodeAt();
+    // get the char code of the current letter
+    const current = array[i].charCodeAt();
+    
+    if (current - prev !== 1) { // if the difference between current and previous is not 1
+      // get the character after the previous 
+      return String.fromCharCode(prev + 1);
+    }
+  }
+  
+  return null; 
+}
